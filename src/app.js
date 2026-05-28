b8897c9715cf8c908561a530974df4f304fb74d4
janeckiisa <janeckiisabelle@gmail.com>
2026-05-28 17:58:08 -0300

    Ajustes na app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,39 @@ const documentoVeiculoRoutes = require(
 )
 
 const app = express()
+const express = require("express")
+const cors = require("cors")
 
+const userRoutes = require("./routes/userRoutes")
+const authRoutes = require("./routes/authRoutes")
+const veiculoRoutes = require("./routes/veiculoRoutes")
+const documentoVeiculoRoutes = require(
+    "./routes/documentoVeiculoRoutes"
+)
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+
+    res.send("API funcionando")
+
+})
+
+app.use("/users", userRoutes)
+
+app.use("/auth", authRoutes)
+
+app.use("/veiculos", veiculoRoutes)
+
+app.use(
+    "/documentos-veiculo",
+    documentoVeiculoRoutes
+)
+
+module.exports = app
 app.use(cors())
 app.use(express.json())
 
