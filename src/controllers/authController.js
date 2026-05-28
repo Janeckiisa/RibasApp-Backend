68a564f54c38889e838f01e72aef69d8af1ccce6
janeckiisa <janeckiisabelle@gmail.com>
2026-05-28 17:21:26 -0300

    Atualização de authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
+const User = require("../models/User")
 const UserLogin = require("../models/UserLogin")
 
 const register = async (req, res) => {
@@ -31,22 +32,33 @@ const register = async (req, res) => {
 const login = async (req, res) => {
 
     try {
+        const { matricula, senha } = req.body
 
-        const { userId, senha } = req.body
-
-        const user = await UserLogin.findOne({
-            userId
+        const usuario = await User.findOne({
+            matricula,
+            isActive: true
         })
 
-        if (!user) {
+        if (!usuario) {
             return res.status(404).json({
                 error: "Usuário não encontrado"
             })
         }
 
+        const userLogin = await UserLogin.findOne({
+            userId: usuario._id,
+            isActive: true
+        })
+
+        if (!userLogin) {
+            return res.status(404).json({
+                error: "Login não encontrado"
+            })
+        }
+
         const senhaValida = await bcrypt.compare(
             senha,
-            user.senha
+            userLogin.senha
         )
 
         if (!senhaValida) {
@@ -57,8 +69,8 @@ const login = async (req, res) => {
 
         const token = jwt.sign(
             {
-                id: user._id,
-                tipo: user.tipoUsuario
+                id: userLogin._id,
+                tipo: userLogin.tipoUsuario
             },
             process.env.JWT_SECRET,
             {
@@ -68,17 +80,21 @@ const login = async (req, res) => {
 
         res.json({
             token,
-            primeiroLogin: user.primeiroLogin
+            primeiroLogin:
+                userLogin.primeiroLogin,
+            usuario: {
+                id: usuario._id,
+                nome: usuario.nome,
+                matricula: usuario.matricula,
+                cargo: usuario.cargo
+            }
         })
 
     } catch (error) {
-
         res.status(500).json({
             error: error.message
         })
-
     }
-
 }
 
 const changePassword = async (req, res) => {
